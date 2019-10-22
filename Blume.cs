9a55a7f3bdd91f68356417afd88b97dd3ed4a1ac
Zeeb <sBWeuzeeb@fh-rosenheim.priv>
2019-10-22 13:23:21 +0200

    Blume und Pflanze erstellt

---
+++ Blume.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prg3Uebung02
+{
+    class Blume: Pflanze
+    {
+
+        private int farbe;
+        Blume(String name, int hoehe, int bodenart,int farbe): base(name, hoehe, bodenart)
+        {
+            if (farbe == Gruen)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            this.farbe = farbe;
+        }
+
+        protected override int GetFarbe()
+        {
+            return this.farbe;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() * 23 + this.GetFarbe();
+        }
+
+        public override bool Equals(object value)
+        {
+            Blume obj = value as Blume;
+            // Is null?
+            if (Object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            // Is the same object?
+            if (Object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            // Is the same type?
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.bodenart == obj.bodenart && this.name.Equals(obj.name) && this.hoehe == obj.hoehe && this.GetFarbe() == obj.GetFarbe();
+        }
+    }
+}
---
+++ Pflanze.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prg3Uebung02
+{
+    abstract class Pflanze
+    {
+
+        protected String name;
+        protected int hoehe;
+        protected int bodenart;
+
+        private const int Sand = 1;
+        private const int Lehm = 2;
+        private const int Humus = 3;
+
+        protected const int Rot = 1;
+        protected const int Gelb = 2;
+        protected const int Weiss = 3;
+        protected const int Orange = 4;
+        protected const int Gruen = 5;
+
+        protected Pflanze(String name, int hoehe, int bodenart)
+        {
+            this.name = name;
+            this.hoehe = hoehe;
+            this.bodenart = bodenart;
+        }
+
+        public String _name { get; set; }
+        public int _hoehe { get; set; }
+        public int _bodenart { get; set; }
+
+        protected abstract int GetFarbe();
+
+        public override bool Equals(object value)
+        {
+            Pflanze obj = value as Pflanze;
+            // Is null?
+            if (Object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            // Is the same object?
+            if (Object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            // Is the same type?
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.bodenart == obj.bodenart && this.name.Equals(obj.name) && this.hoehe == obj.hoehe;
+        }
+
+
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+
+            hash = hash * 23 + this.name.GetHashCode();
+            hash = hash * 23 + this.bodenart;
+            hash = hash * 23 + this.hoehe;
+            return hash;
+        }
+    }
+}
