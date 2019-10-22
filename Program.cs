3e049636af50315c6ea4c6057856205b434a5f73
Zeeb <sBWeuzeeb@fh-rosenheim.priv>
2019-10-22 11:53:13 +0200

    Initial program setUp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prg3Uebung02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
